f43f0f519db91dc11fab77588cc98f191352ab5b
Tigran <tik.sargsyan105@gmail.com>
2022-03-10 18:31:06 +0400

    gloabal change redux end

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,13 @@
+import React, {FC} from 'react';
+
+const TodoList: FC = () => {
+    // TIKO tpelu paherna 33.18
+    //https://www.youtube.com/watch?v=ETWABFYv0GM
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default TodoList;
